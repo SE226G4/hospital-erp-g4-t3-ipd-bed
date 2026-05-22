9ee64a3e292e4ba279b1ab1656cbca43668af90b
Mahmoud Alshbly <shbly1029@gmail.com>
2026-05-22 13:38:18 -0700

    Update sequence diagram and add check bed suitably

--- HospitalSystem/obj/Debug/net8.0/HospitalSystem.AssemblyInfo.cs
+++ HospitalSystem/obj/Debug/net8.0/HospitalSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HospitalSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a00cda21139dd1c9d913908f8344d925be25aaf9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fff1a4de3fbbca5cdf2d7906d496b3421dd1696b")]
 [assembly: System.Reflection.AssemblyProductAttribute("HospitalSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HospitalSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
